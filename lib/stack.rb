88426f6e28cecf85b787cc7ebefaf45b6018c48d
Lucas Duncan <lucasdonuts@gmail.com>
2022-09-01 13:10:50 -0500

    Core deliverables complete

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,21 @@
-# your code here
+class Stack
+  attr_reader :stack
+  
+  def initialize
+    @stack = []
+  end
+
+  def push(value)
+    stack << value
+    self
+  end
+
+  def pop
+    stack.delete_at(-1)
+  end
+
+  def peek
+    stack[-1]
+  end
+
+end
